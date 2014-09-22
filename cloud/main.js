5d2d08507c0fe7cc53eb6294c8756b3314c770cd
Qihe Bian <ufosky@gmail.com>
2014-09-22 09:21:01 +0800

    Fix error.

--- cloud/main.js
+++ cloud/main.js
@@ -2,7 +2,7 @@
 var debug = require('debug')('express_test');
 var app = require('cloud/app.js');
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', 3000);
 
 var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
